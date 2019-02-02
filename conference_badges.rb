8198cea245c99421fd4cd5d04666fa509abfd743
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-02 14:33:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,5 +14,6 @@ def assign_rooms(array)
   room_array = []
   array.each do |name|
     room_assignment = name.index + 1
+    puts "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
   end
 end
